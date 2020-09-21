40beafb35fa2c479545898bf1fa1a352ea9314d9
Igor Doroshenko <oknehsorod@gmail.com>
2020-09-21 22:19:17 +0300

    uuu

---
+++ build/precache-manifest.d6d988e21791e9fee92b2ee5865f9903.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3301d907167d097d83b44c664f874791",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b8d918d115713cc2f80b",
+    "url": "/static/js/2.fd14e33e.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.fd14e33e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "16a37bd3f0e387777ca2",
+    "url": "/static/js/main.b1c9de1a.chunk.js"
+  },
+  {
+    "revision": "af99edab7c3a110db6a6",
+    "url": "/static/js/runtime-main.4bbf50f9.js"
+  }
+]);
